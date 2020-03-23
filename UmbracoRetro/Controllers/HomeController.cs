8cb12f623fc7c4d906bb2ddc083b5d1685b6dc1b
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-23 21:27:30 +0000

    add featured partial view

---
+++ UmbracoRetro/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoRetro.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+         public ActionResult RenderFeatured()
+        {
+            return PartialView("~/Views/Partials/Home/_Featured.cshtml");
+        }
+    }
+}
