4db06fc9dda897d3ec66371c400aef933da14830
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-23 20:10:43 +0000

    add header and footer partial view

---
+++ UmbracoRetro/Controllers/SiteLayoutController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoRetro.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        public ActionResult RenderHeader()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/_Header.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/_Footer.cshtml");
+        }
+    }
+}
