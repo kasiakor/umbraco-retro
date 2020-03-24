a28073c3e9485ff17612dbab7331e47725cab6da
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-24 20:36:52 +0000

    add clients blog and services partial view

--- UmbracoRetro/Controllers/HomeController.cs
+++ UmbracoRetro/Controllers/HomeController.cs
@@ -9,5 +9,20 @@ namespace UmbracoRetro.Controllers
         {
             return PartialView("~/Views/Partials/Home/_Featured.cshtml");
         }
+
+        public ActionResult RenderServices()
+        {
+            return PartialView("~/Views/Partials/Home/_Services.cshtml");
+        }
+
+        public ActionResult RenderBlog()
+        {
+            return PartialView("~/Views/Partials/Home/_Blog.cshtml");
+        }
+
+        public ActionResult RenderClients()
+        {
+            return PartialView("~/Views/Partials/Home/_Clients.cshtml");
+        }
     }
 }
