d222f31e957eb8bc3845b9c482fb644dbdd40d06
Kasia Kordys <kasia.kordys@gmail.com>
2020-03-31 22:04:25 +0100

    add blog model and controller

---
+++ UmbracoRetro/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+
+
+namespace UmbracoRetro.Controllers
+{
+    public class BlogController : SurfaceController
+    {
+        public ActionResult RenderPostList()
+        {
+            return PartialView("~/Views/Partials/Blog/_PostList.cshtml");
+        }
+    }
+}
