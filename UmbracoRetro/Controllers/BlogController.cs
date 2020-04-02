508ed90034bf41e51dbff05696e50b1068e12e9c
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-02 17:10:22 +0100

    add limit to rendered articles

--- UmbracoRetro/Controllers/BlogController.cs
+++ UmbracoRetro/Controllers/BlogController.cs
@@ -10,18 +10,19 @@ namespace UmbracoRetro.Controllers
 {
     public class BlogController : SurfaceController
     {
-        public ActionResult RenderPostList()
+        public ActionResult RenderPostList(int numberOfItems)
         {
-      
             List<BlogPreview> model = new List<BlogPreview>();
+            IPublishedContent blogPage = CurrentPage.AncestorOrSelf(1).DescendantsOrSelf().Where(x => x.DocumentTypeAlias == "blog").FirstOrDefault();
+           
             //code to get the current page using helper
             //UmbracoHelper umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
             //IPublishedContent blogPage = CurrentPage.AncestorOrSelf("blog");
-            IPublishedContent blogPage = Umbraco.AssignedContentItem.AncestorOrSelf("blog");
+            //IPublishedContent blogPage = Umbraco.AssignedContentItem.AncestorOrSelf("blog");
 
             if (blogPage != null)
             {
-                foreach (IPublishedContent page in blogPage.Children.OrderByDescending(x => x.UpdateDate))
+                foreach (IPublishedContent page in blogPage.Children.OrderByDescending(x => x.UpdateDate).Take(numberOfItems))
                 {
                     var imageId = page.GetPropertyValue<string>("articleImage");
     
