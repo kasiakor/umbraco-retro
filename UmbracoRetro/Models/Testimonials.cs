31c5b79767346df0c54a41d86ccfdb65de4b0a15
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-03 18:58:09 +0100

    modify testimonials model to address the list of testimonials

--- UmbracoRetro/Models/Testimonials.cs
+++ UmbracoRetro/Models/Testimonials.cs
@@ -9,6 +9,26 @@ namespace UmbracoRetro.Models
     {
         public string Title { get; set; }
         public string Introduction { get; set; }
+        public List<Testimonial> Testimonialslist { get; set; }
+        public bool HasTestimonials { get { return Testimonialslist != null && Testimonialslist.Count > 0; } }
+        public string ColumnClass
+        {
+            get
+            {
+                switch (Testimonialslist.Count)
+                {
+                    case 2:
+                        return "col-md-6";
+                    case 3:
+                        return "col-md-4";
+                    case 4:
+                        return "col-md-3";
+                    default:
+                        return "col-md-12";
+                }
+            }
+        } 
+
 
         public Testimonials(string title, string introducion)
         {
