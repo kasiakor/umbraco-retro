34eebe5f73b92abee469fa2bca255bc8444b8955
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-07 21:46:57 +0100

    add home helper

--- UmbracoRetro.Library/Helpers/HomeHelper.cs
+++ UmbracoRetro.Library/Helpers/HomeHelper.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 
 namespace UmbracoRetro.Library.Helpers
 {
     public class HomeHelper
     {
-        public HomeHelper()
+
+        private IPublishedContent _currentPage;
+        private UmbracoHelper _uHelper;
+        // with UmbracoHelper we can use Umbraco.Content or Umbraco.Media
+        public HomeHelper(IPublishedContent currentPage, UmbracoHelper uHelper)
         {
+            _currentPage = currentPage;
+            _uHelper = uHelper;
 
         }
     }
